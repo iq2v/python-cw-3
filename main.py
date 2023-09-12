dd1a1f486909ade63416804d0c9b36bf5ff850a2
iq2v <142683864+iq2v@users.noreply.github.com>
2023-09-12 22:39:00 +0300

    3

---
+++ main.py
@@ -0,0 +1,21 @@
+# الجزء الاول
+favorite_animals = ['dogs' ,'cats','monkeys','rabbits']
+print(favorite_animals)
+print(favorite_animals[1])
+favorite_animals.remove('monkeys')
+
+#الجزء الثاني
+favorite_animals.append('foxes')
+for animals in favorite_animals :
+    print(f'i love {animals}')
+    
+
+
+
+#الجزء الثالث
+
+numbers = [5 ,4 ,3 ,2 ,1 ]
+number_sum = 0
+for num in numbers :
+    number_sum += num
+print(number_sum)
